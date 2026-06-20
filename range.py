f8c0d14ca6b6cdead6c02234b3bf0b252c740657
sujon-258549 <sujon258549@gmail.com>
2026-06-20 20:34:28 +0600

    Add range function implementation to generate a sequence of numbers

---
+++ range.py
@@ -0,0 +1,5 @@
+
+# range() function is used to generate a sequence of numbers. It takes three arguments: start, stop, and step. The start argument is the first number in the sequence, the stop argument is the last number in the sequence (exclusive), and the step argument is the difference between each number in the sequence.
+a=list(range(10 , 100 ,-5))
+
+print(a)
