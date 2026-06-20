7d9cdc849a7047e7dc0263638e9e609de28f7996
sujon-258549 <sujon258549@gmail.com>
2026-06-20 20:25:50 +0600

    Add initial implementation of list and tuple demonstration

---
+++ listAndTuple.py
@@ -0,0 +1,13 @@
+# What is List and Tuple in Python?
+
+numberofStudentList=[10,20,30,40,5010,60,70,80,90,100]
+
+a="Hellow Bangladesh"
+
+
+# tuple is a collection which is ordered and unchangeable. in python tuple is written with round brackets.
+
+studentTuple=(10,20,30,40,50,60,70,80,90,100)
+
+
+print(list(reversed(studentTuple)))
