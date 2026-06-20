f8dcf4d8434340b56638cedca30454cb7027722e
sujon-258549 <sujon258549@gmail.com>
2026-06-20 19:51:32 +0600

    Add conditional statement to evaluate price

---
+++ conditionalStatement.py
@@ -0,0 +1,11 @@
+
+price=10
+
+if price>10:
+    print("Price is greater than to 10")
+
+elif price==10:
+        print("Price is equal to 10")
+
+else:
+    print("Price is less than or equal to 10")
