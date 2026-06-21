8d7ed5545aa9603a78412cc373134e39781edb7d
sujon-258549 <sujon258549@gmail.com>
2026-06-21 08:24:40 +0600

    Add initial implementations of dictionary, function definitions, keyword arguments, and lambda function

---
+++ function.py
@@ -0,0 +1,20 @@
+
+# user defined function
+# build in function
+
+
+# user defined function
+def my_function(name):
+  print("My name is {}".format(name))
+
+
+
+hellow=my_function("Alice")
+
+hellow=my_function("Bob")
+
+# Multiple Arguments
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
