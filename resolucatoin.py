bc2380a084b49e5c1d1f3a16089e70f5745fdb7a
sujon-258549 <sujon258549@gmail.com>
2026-06-21 21:59:03 +0600

    Add initial implementation of resolution explanation and function to print name

---
+++ resolucatoin.py
@@ -0,0 +1,9 @@
+# what is resolution?
+# Resolution refers to the level of detail or clarity in an image or display. It is typically measured in pixels per inch (PPI) or dots per inch (DPI). A higher resolution means more pixels or dots are packed into each inch, resulting in a sharper and more detailed image.
+
+name="sujon"
+
+def my_function():
+  print("My name is {}".format(name))
+
+my_function()
